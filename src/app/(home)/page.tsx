e5a28d11da585eb59161c74691d5e1cf2eeb4f70
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-01 20:10:45 +0530

    style: add metadata

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,13 @@
 import Hero from '@/components/Hero';
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	openGraph: {
+		title: "Reviactyl - Free Theme For Pterodactyl",
+		description: "Reviactyl is an open, fast, and free pterodactyl theme. It's the first and only open source Pterodactyl Theme to have features that even paid themes don't.",
+		images: [{ url: "/banner.png" }],
+	},
+};
 
 export default function HomePage() {
   return (
