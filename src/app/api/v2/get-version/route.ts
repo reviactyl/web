5588398a6aeb293012491ff14e724e612aca6497
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-03 13:38:49 +0530

    chore: bump reviactyl version

--- src/app/api/v2/get-version/route.ts
+++ src/app/api/v2/get-version/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json({
     status: "OK",
-    panel: "2.0.0",
+    panel: "2.0.1",
     wings: "1.11.13",
     sftp: "1.0.5",
     discord: "https://reviactyl.dev/discord",
