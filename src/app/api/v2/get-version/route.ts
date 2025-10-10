7c35b7aaf05a852f13e14e0c23204f0122132448
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-10-10 20:44:59 +0530

    update panel version

--- src/app/api/v2/get-version/route.ts
+++ src/app/api/v2/get-version/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json({
     status: "OK",
-    panel: "2.0.1",
+    panel: "2.0.2",
     wings: "1.11.13",
     sftp: "1.0.5",
     discord: "https://reviactyl.dev/discord",
