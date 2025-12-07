18bd9a272d6a10c4082ab6c519a0b4b8b764c0d4
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-12-07 19:02:48 +0530

    Update blog post title and blog page layout
    Changed the blog post title to reflect the new development halt date. Refactored the blog post page layout for improved visual hierarchy, separating the title, image, and content into distinct sections with updated styling.

--- src/app/(home)/blog/[slug]/page.tsx
+++ src/app/(home)/blog/[slug]/page.tsx
@@ -18,23 +18,17 @@ export default async function Page(props: {
     <main className="relative h-full w-full">
       <div className="absolute inset-0 z-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
       <div className="relative z-10 border border-black/20 dark:border-zinc-800 m-4 rounded-xl">
-      <div className="rounded-xl border py-20 md:px-8" style={{backgroundImage: `url(${page.data.image})`,backgroundPosition: "center",backgroundSize: "cover",backgroundRepeat: "no-repeat",}}>
-        <h1 className="mb-2 text-3xl font-bold text-shadow-xl text-white">{page.data.title}</h1>
-        <p className="mb-4 text-gray-200 text-shadow-xl">{page.data.description}</p>
-          <Link
-            href="/blog"
-            className="inline-flex items-center justify-center text-sm font-medium ring-offset-fd-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-fd-ring disabled:pointer-events-none disabled:opacity-50 border hover:bg-fd-accent h-8 px-3 rounded-xl bg-fd-background"
-          >
-            <span className="relative flex items-center justify-center gap-2 hover:text-fd-accent-foreground">
-              Back
-            </span>
-          </Link>
-      </div>
-      <article className="flex flex-col mx-auto w-full py-8 lg:flex-row bg-gradient-to-b from-fd-background/80 to-fd-background border-t border-b rounded-xl border-black/20 dark:border-zinc-800 mt-2">
-        <div className="prose min-w-0 flex-1 p-4">
+      <article className="flex flex-col mx-auto w-full lg:flex-row gap-x-2">
+        <div className="prose min-w-0 flex-1 gap-y-2">
+          <div className="py-12 px-4 bg-gradient-to-b from-fd-background/80 to-fd-background border-t border-b rounded-xl border-black/20 dark:border-zinc-800">
+        <h1 className="mb-2 text-6xl font-semibold">{page.data.title}</h1>
+          </div>
+          <img className="border-t border-b rounded-xl border-black/20 dark:border-zinc-800" src={page.data.image} alt={page.data.title} />
+          <div className="p-4 !pt-0 bg-gradient-to-b from-fd-background/80 to-fd-background border-t border-b rounded-xl border-black/20 dark:border-zinc-800">
           <Mdx components={defaultMdxComponents} />
+          </div>
         </div>
-        <div className="flex flex-col gap-4 border-l p-4 text-sm lg:w-[250px]">
+        <div className="flex flex-col gap-4 border-l p-4 text-sm lg:w-[250px] bg-gradient-to-b from-fd-background/80 to-fd-background border-t border-b rounded-xl border-black/20 dark:border-zinc-800">
           <div>
             <InlineTOC items={page.data.toc} />
           </div>
