969984be0740bd43982005712a84bb9927f44857
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-12-27 18:33:28 +0530

    bump panel & cli version

--- src/app/api/v2/get-version/route.ts
+++ src/app/api/v2/get-version/route.ts
@@ -3,10 +3,10 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json({
     status: "OK",
-    panel: "2.1.0",
+    panel: "2.1.1",
     wings: "1.11.13",
     sftp: "1.0.5",
-    cli: "2.1.0",
+    cli: "2.1.1",
     discord: "https://reviactyl.dev/discord",
     donations: "https://github.com/sponsors/reviactyl",
   });
