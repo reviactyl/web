c2a1a04ae2e92031358183955526edd110f52e14
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-01-03 15:31:35 +0530

    2026!

--- src/app/(home)/Footer.tsx
+++ src/app/(home)/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="relative z-10 border border-black/20 dark:border-zinc-800 mt-4 rounded-xl bg-fd-background">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-       <span className="text-sm sm:text-center">© 2025 Reviactyl&trade; & Contributors.</span>
+       <span className="text-sm sm:text-center">© 2026 Reviactyl&trade; & Contributors.</span>
       </div>
     </footer>
   );
