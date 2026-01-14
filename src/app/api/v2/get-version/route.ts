c2ee6c51c4c32aaa448cbae612cc2ccf27127a82
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-01-14 14:37:01 +0530

    Update version numbers in GET response

--- src/app/api/v2/get-version/route.ts
+++ src/app/api/v2/get-version/route.ts
@@ -3,8 +3,8 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json({
     status: "OK",
-    panel: "2.1.2",
-    wings: "1.11.13",
+    panel: "2.2.0",
+    wings: "1.12.1",
     sftp: "1.0.5",
     cli: "2.1.1",
     discord: "https://reviactyl.dev/discord",
