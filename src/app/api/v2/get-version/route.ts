4f0d00ec3234d70da5de10f8c40c794c4d0c37c7
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-01-29 17:23:22 +0530

    bump panel version

--- src/app/api/v2/get-version/route.ts
+++ src/app/api/v2/get-version/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json({
     status: "OK",
-    panel: "2.2.0",
+    panel: "2.2.1",
     wings: "1.12.1",
     sftp: "1.0.5",
     cli: "2.1.1",
