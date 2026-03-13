5f42ac0a1c96a4a9665ad80aea940ec021f45867
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-03-13 19:18:46 +0530

    Update panel version to 2.2.3
    too late tbh

--- src/app/api/v2/get-version/route.ts
+++ src/app/api/v2/get-version/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json({
     status: "OK",
-    panel: "2.2.2",
+    panel: "2.2.3",
     wings: "1.12.1",
     sftp: "1.0.5",
     cli: "2.1.1",
