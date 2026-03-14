6ab3a5b04bd19c1b2df5f48fc26c6663bef3d807
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-03-14 15:50:48 +0530

    better styling for Hero section

--- src/app/(home)/Hero.tsx
+++ src/app/(home)/Hero.tsx
@@ -1,20 +1,26 @@
-import Image from "next/image";
-import PanelStars from "@/components/PanelStars";
+"use client"
 import PanelVersion from "@/components/PanelVersion";
 import Link from "next/link";
-import React from "react";
-import { FaArrowRight, FaBookMedical, FaGithub } from "react-icons/fa";
-import { FaStar } from "react-icons/fa6";
+import React, { useEffect, useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 export default function Hero() {
+  const [version, setVersion] = useState("v0");
+
+  useEffect(() => {
+    fetch("/api/v26/get-latest")
+      .then(res => res.json())
+      .then(data => setVersion(data.version_number))
+      .catch(() => setVersion("v0"));
+  }, []);
   return (
     <section className="bg-[radial-gradient(125%_125%_at_50%_10%,_#ffffff_40%,_#ffcccc_100%)] dark:bg-[radial-gradient(125%_125%_at_50%_10%,_#000000_40%,_#2b0707_100%)] rounded-xl">
       <div className="mx-auto max-w-screen-xl px-4 pt-10 lg:pt-16 lg:px-12 flex flex-col items-center text-center">
-        <Link href="/docs/blueprint" className="inline-flex items-center px-4 py-2 rounded-full border border-neutral-200 bg-white px-4 py-1.5 text-sm font-medium text-neutral-700 shadow-sm dark:border-neutral-800 dark:bg-neutral-900/50 dark:text-neutral-300 mb-8">
+        <Link href={`https://github.com/reviactyl/panel/releases/tag/${version}`} className="inline-flex items-center px-4 py-2 rounded-full border border-neutral-200 bg-white px-4 py-1.5 text-sm font-medium text-neutral-700 shadow-sm dark:border-neutral-800 dark:bg-neutral-900/50 dark:text-neutral-300 mb-8">
           <span className="animate-pulse mr-2 h-2 w-2 bg-blue-600 rounded-full" />
-          <span>Reviactyl now supports blueprint-addons!</span>
+          <span>Panel <PanelVersion /> has been released!</span>
         </Link>
-        <h1 className="text-5xl font-bold tracking-tight text-neutral-900 dark:text-white sm:text-6xl lg:text-7xl">
+        <h1 className="text-5xl font-bold tracking-tight text-neutral-900 dark:text-white sm:text-6xl lg:text-[5.5rem] lg:leading-[1.0]">
           <span className="block md:hidden">
             Pterodactyl<br />
             <span className="bg-gradient-to-r from-neutral-600 to-blue-600 bg-clip-text text-transparent dark:from-neutral-400 dark:to-blue-400">
@@ -24,7 +30,7 @@ export default function Hero() {
 
           <span className="hidden md:block">
             Manage Game Servers<br />
-            <span className="bg-gradient-to-r from-neutral-600 to-blue-600 bg-clip-text text-transparent dark:from-neutral-400 dark:to-blue-400">
+            <span className="bg-gradient-to-br from-white via-blue-300 to-blue-400 bg-clip-text text-transparent dark:from-neutral-400 dark:to-blue-400">
               Like Never Before!
             </span>
           </span>
@@ -33,49 +39,25 @@ export default function Hero() {
           <span className="md:hidden">
             Manage Game Servers like never before
           </span>
-          <span className="hidden md:inline relative group cursor-help">
-            Feature-packed, compatible with pterodactyl-native* and blueprint* addons, and more powerful than other forks. What else you need?
-             <span className="pointer-events-none absolute bottom-full left-1/2 mb-2 hidden -translate-x-1/2 rounded-xl bg-white/80 dark:bg-black/80 border border-white dark:border-black px-2 py-1 text-xs text-white group-hover:block">
-             Reviactyl supports most of the addons out of the box. But some addons may conflict and not render properly, before you go on and blame on addon developers please take a help from us. Blueprint or Pterodactyl won&apos;t provide support for issue with ReBlueprint or Reviactyl. You have to ask for help at your discord server directly.
-             </span>
+          <span className="hidden md:inline">
+            Designed with security in mind, Reviactyl runs all game servers in isolated Docker containers while exposing a beautiful and intuitive UI to end users.
           </span>
-        </p>
-
-        <p className="hidden md:inline mt-2 max-w-3xl leading-relaxed text-neutral-600 dark:text-neutral-400 text-md">
-          The latest version is <span className="font-semibold text-black dark:text-white">Panel <PanelVersion /></span>
         </p>
 
         <div className="mt-8 mb-8 flex flex-col sm:flex-row gap-4">
           <Link
             href="/docs"
-            className="inline-flex items-center justify-center text-sm font-medium ring-offset-fd-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-fd-ring disabled:pointer-events-none disabled:opacity-50 bg-gradient-to-b from-fd-primary to-fd-primary/60 text-fd-primary-foreground shadow-fd-background/20 hover:bg-fd-primary/90 h-11 px-6 rounded-full"
+            className="group relative inline-flex h-[calc(48px+8px)] cursor-pointer items-center justify-center rounded-full bg-fd-secondary/70 py-1 pl-6 text-lg shadow-lg pr-14"
           >
-            <span className="relative flex items-center justify-center gap-2 text-white dark:text-black">
-              <FaBookMedical /> Documentation
+            <span className="relative flex items-center justify-center gap-2 text-fd-accent-foreground">
+              Get Started <span className="border border-gray-300 dark:border-gray-600 px-2 py-1 rounded-xl text-sm font-bold">Free</span>
             </span>
+            <div className="absolute right-1 inline-flex h-12 w-12 items-center justify-end rounded-full border bg-fd-accent transition-[width] group-hover:w-[calc(100%-8px)]">
+              <div className="mr-3.5 flex items-center justify-center">
+                <FaArrowRight className="h-5 w-5 text-fd-accent-foreground" />
+              </div>
+            </div>
           </Link>
-          <div className="flex flex-col items-center gap-1">
-          <Link
-            href="https://github.com/reviactyl/panel"
-            className="inline-flex items-center justify-center text-sm font-medium ring-offset-fd-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-fd-ring disabled:pointer-events-none disabled:opacity-50 border hover:bg-fd-accent h-11 px-6 rounded-full bg-fd-background"
-          >
-            <span className="relative flex items-center justify-center gap-2 hover:text-fd-accent-foreground">
-              <FaGithub /> Star on Github <FaArrowRight className="text-gray-500" />
-            </span>
-          </Link>
-          <div className="inline-flex text-sm items-center gap-1 text-black/50 dark:text-white/50">
-          <FaStar className="text-yellow-500" /> <span className="text-black dark:text-white font-bold"><PanelStars /></span> stars on github.
-          </div>
-          </div>
-        </div>
-        <div className="relative inline-block group">
-          <Image
-            className="mx-auto rounded-lg shadow-xl border-gray-800 z-1"
-            src="/dashboard.png"
-            alt="preview"
-            width={800}
-            height={600}
-          />
         </div>
       </div>
     </section>
