45ebd37ac36bc95ccacc34121b18a0f8ec5a3fa3
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-03-15 08:25:22 +0530

    add logo for Hero section
    * with in motion

--- src/app/(home)/Hero.tsx
+++ src/app/(home)/Hero.tsx
@@ -1,27 +1,46 @@
 "use client"
 import PanelInstalls from "@/components/PanelInstalls";
-import PanelVersion from "@/components/PanelVersion";
+import { motion } from "framer-motion";
+import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 import { FaArrowTurnUp } from "react-icons/fa6";
 
 export default function Hero() {
-  const [version, setVersion] = useState("v0");
-
-  useEffect(() => {
-    fetch("/api/v26/get-latest")
-      .then(res => res.json())
-      .then(data => setVersion(data.version_number))
-      .catch(() => setVersion("v0"));
-  }, []);
+//  const [version, setVersion] = useState("v0");
+//
+//  useEffect(() => {
+//    fetch("/api/v26/get-latest")
+//      .then(res => res.json())
+//      .then(data => setVersion(data.version_number))
+//      .catch(() => setVersion("v0"));
+//  }, []);
   return (
     <section className="bg-[radial-gradient(125%_125%_at_50%_10%,_#ffffff_40%,_#ffcccc_100%)] dark:bg-[radial-gradient(125%_125%_at_50%_10%,_#000000_40%,_#2b0707_100%)] rounded-xl">
       <div className="mx-auto max-w-screen-xl px-4 pt-10 lg:pt-16 lg:px-12 flex flex-col items-center text-center">
-        <Link href={`https://github.com/reviactyl/panel/releases/tag/${version}`} className="inline-flex items-center px-4 py-2 rounded-full border border-neutral-200 bg-white px-4 py-1.5 text-sm font-medium text-neutral-700 shadow-sm dark:border-neutral-800 dark:bg-neutral-900/50 dark:text-neutral-300 mb-8">
-          <span className="animate-pulse mr-2 h-2 w-2 bg-blue-600 rounded-full" />
-          <span>Panel <PanelVersion /> has been released!</span>
-        </Link>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
+        <Image
+          src="/logo.png"
+          alt="Reviactyl Logo"
+          width={276}
+          height={64}
+          priority
+          className="mb-3 h-15 hidden dark:block w-auto rounded-full"
+        />
+        <Image
+          src="/logo-darker.png"
+          alt="Reviactyl Logo"
+          width={276}
+          height={64}
+          priority
+          className="mb-3 h-15 block dark:hidden w-auto rounded-full"
+        />
+        </motion.div>
         <h1 className="text-5xl font-bold tracking-tight text-neutral-900 dark:text-white sm:text-6xl lg:text-[5.5rem] lg:leading-[1.0]">
           <span className="block md:hidden">
             Pterodactyl<br />
@@ -61,7 +80,7 @@ export default function Hero() {
             </div>
           </Link>
           <div className="inline-flex text-sm items-center gap-1 text-black/50 dark:text-white/50">
-            <span className="text-black dark:text-white font-bold"><PanelInstalls /></span> total installations
+            <span className="text-black dark:text-white font-bold"><PanelInstalls />+ panels</span> are using Reviactyl
             <FaArrowTurnUp />
           </div>
         </div>
