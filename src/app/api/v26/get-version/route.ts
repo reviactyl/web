39953dec1e3136779b85a73703770894b30678c7
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-08-01 21:42:13 +0530

    update software versions

--- src/app/api/v26/get-version/route.ts
+++ src/app/api/v26/get-version/route.ts
@@ -3,8 +3,8 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json({
     status: "OK",
-    panel: "26.06.0",
-    agent: "26.06.0",
+    panel: "26.08.0",
+    agent: "26.08.0",
     sftp: "1.0.5",
     cli: "2.1.1",
     discord: "https://reviactyl.app/discord",
