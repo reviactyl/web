fc4316227db3d8d5f2062ee68ba19051c743b301
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-09-25 19:39:21 +0530

    style: remove edge margins from page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -19,8 +19,6 @@ export const metadata: Metadata = {
 export default function HomePage() {
   return (
     <main className="relative h-full w-full">
-      <div className="absolute inset-0 z-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
-      <div className="relative z-10 border border-black/20 dark:border-zinc-800 m-4 rounded-xl">
       <Hero />
       <Info />
       <Features />
@@ -28,7 +26,6 @@ export default function HomePage() {
       <Extensions />
       <Sponsors />
       <End />
-      </div>
       <Footer />
     </main>
   );
